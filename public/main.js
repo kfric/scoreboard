da83173b09764bee63e2ef9c1953751f0aac6e49
Karl Frick <karl.frick5@gmail.com>
2021-05-19 19:40:15 -0400

    Reset button resets scores to 0. Added team2 data to mirror team1

--- public/main.js
+++ public/main.js
@@ -8,38 +8,83 @@ function main() {
     .querySelector('.team1 input')
     .addEventListener('input', updateTeam1Name)
 
-  let moveCounter = 0
+  // replaces value of section team2 h2 to the value of .team2
+  function updateTeam2Name(event) {
+    const teamName = document.querySelector('.team2 h2')
+    teamName.textContent = event.target.value
+  }
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', updateTeam2Name)
+
+  // first team totals
+  let teamOneCounter = 0
   const team1Name = document.querySelector('.team1 h2')
-  function updateScoreAdd() {
-    if (moveCounter >= 21) {
+  function teamOneScoreAdd() {
+    if (teamOneCounter >= 21) {
       window.alert(`${team1Name.textContent} WINS!`)
       return
     }
-    moveCounter++
+    teamOneCounter++
     const teamOneScore = document.querySelector('.team1 h3')
-    teamOneScore.textContent = moveCounter
+    teamOneScore.textContent = teamOneCounter
   }
-  document.querySelector('.add').addEventListener('click', updateScoreAdd)
+  document
+    .querySelector('.team1 .add')
+    .addEventListener('click', teamOneScoreAdd)
 
-  moveCounter = 0
-  function updateScoreSub() {
-    if (moveCounter <= 0) {
+  function TeamOneScoreSub() {
+    if (teamOneCounter <= 0) {
       window.alert('NO SUCH THING AS A NEGATIVE SCORE...')
       return
     }
-    moveCounter--
+    teamOneCounter--
     const teamOneScore = document.querySelector('.team1 h3')
-    teamOneScore.textContent = moveCounter
+    teamOneScore.textContent = teamOneCounter
   }
-  document.querySelector('.subtract').addEventListener('click', updateScoreSub)
+  document
+    .querySelector('.team1 .subtract')
+    .addEventListener('click', TeamOneScoreSub)
+
+  // Second team totals
+  let teamTwoCounter = 0
+  const team2Name = document.querySelector('.team2 h2')
+  function teamTwoScoreAdd() {
+    if (teamTwoCounter >= 21) {
+      window.alert(`${team2Name.textContent} WINS!`)
+      return
+    }
+    teamTwoCounter++
+    const teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = teamTwoCounter
+  }
+  document
+    .querySelector('.team2 .add')
+    .addEventListener('click', teamTwoScoreAdd)
+
+  function TeamTwoScoreSub() {
+    if (teamTwoCounter <= 0) {
+      window.alert('NO SUCH THING AS A NEGATIVE SCORE...')
+      return
+    }
+    teamTwoCounter--
+    const teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = teamTwoCounter
+  }
+  document
+    .querySelector('.team2 .subtract')
+    .addEventListener('click', TeamTwoScoreSub)
 
   // reset button
-  const resetScore = 0
   function resetScores() {
     teamOneScore = document.querySelector('.team1 h3')
-    teamOneScore.textContent = resetScore
+    teamOneScore.textContent = 0
+    teamOneCounter = 0
+    teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = 0
+    teamTwoCounter = 0
   }
-  document.querySelector('footer').addEventListener('click', resetScores)
+  document.querySelector('a').addEventListener('click', resetScores)
 }
 
 document.addEventListener('DOMContentLoaded', main)
