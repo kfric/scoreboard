5ed8c611c3ddac30d0aa8f1bc42b89e5c78ef688
Karl Frick <karl.frick5@gmail.com>
2021-05-19 16:16:23 -0400

    Team name replaced with input from user and + - counter live updates

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  // replaces value of section team1 h2 to the value of .team1
+  function updateTeam1Name(event) {
+    const teamName = document.querySelector('.team1 h2')
+    teamName.textContent = event.target.value
   }
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', updateTeam1Name)
+
+  let moveCounter = 0
+  function updateScoreAdd() {
+    moveCounter++
+    const addScore = document.querySelector('.team1 h3')
+    addScore.textContent = moveCounter
+  }
+  document.querySelector('.add').addEventListener('click', updateScoreAdd)
+
+  moveCounter = 0
+  function updateScoreSub() {
+    moveCounter--
+    const subScore = document.querySelector('.team1 h3')
+    subScore.textContent = moveCounter
+  }
+  document.querySelector('.subtract').addEventListener('click', updateScoreSub)
 }
 
 document.addEventListener('DOMContentLoaded', main)
